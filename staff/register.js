d9ec27dc3e9288e58e6465a34ca9af6f414ca55e
Amersant1 <96844840+Amersant1@users.noreply.github.com>
2022-03-07 12:57:34 +0300

    Add files via upload

---
+++ staff/register.js
@@ -0,0 +1,29 @@
+const URL_FOR_REGIST="http://127.0.0.1:5000/newClient/"
+
+document.getElementById("register").onclick=function(){
+    document.getElementById("regist_form").classList.toggle("hidden")
+}
+document.getElementById("sending").onclick=async function(){
+    document.getElementById("wrongphone").classList.add("hidden")
+
+    console.log("done")
+    name1=document.getElementsByClassName("register_input")[0].value
+    phone=document.getElementsByClassName("register_input")[1].value
+    if (validation(phone)==true){
+    url=URL_FOR_REGIST+name1+"&&"+phone
+    resp=await fetch(url)
+    resp1=await fetch(URL_FOR_REGIST+"generate")
+    resp_final=await resp1.json()
+    console.log(resp_final["text"])
+    console.log(resp_final)
+    resp_final=resp_final["text"]
+    window.open("returns/"+resp_final+".html")}
+    else{
+        document.getElementById("wrongphone").classList.remove("hidden")
+    }
+
+}
+function validation(phone){
+    return(checkphone(phone))
+    
+}
---
+++ staff/server.py
@@ -0,0 +1,52 @@
+import flask
+from flask import Flask
+from flask import jsonify, Request
+from flask_cors import CORS
+import requests
+import json
+import time
+import random
+import string
+import math
+def generator():
+    length=15
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+    print("Random string of length", length, "is:", rand_string)
+app = Flask(__name__)
+CORS(app)
+
+URL = 'https://api.shiptor.ru/shipping/v1'
+headers = {
+        'Content-Type' : 'application/json',
+        'X-Authorization-Token' : '05eaca80d8fbc53660329ccc6ce97b3da3823788'
+    }
+
+@app.route('/newClient/<name>&&<phone>')
+def search_city(name,phone):
+    print(name,phone)
+    return ({"name":name,"phone":phone})
+@app.route("/newClient/generate")
+def generate():
+    length=15
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+    x=open("../returns/"+rand_string+".html","w")
+    g=open("../templ_for_registr.html","r")
+    html=g.read()
+    print(html,file=x)
+    x.close()
+    g.close()
+    print(rand_string)
+    # time.sleep(0.1)
+    return ({"text":rand_string})
+
+
+
+
+
+
+
+
+
+app.run(debug=True)
